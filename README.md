062c270fa3604b2b307fe49950e32241859e545b
patricksile <patricksile@gmail.com>
2019-01-03 18:40:54 +0100

    Project 1 first push

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project1: Music Break Project
+
+
+Do you know a friend who works too many hours? We will write a program that schedules
+breaks throughout the day -- reminding your friend to listen to his favorite music in 
+the web browser, get up and dance to it or just walk away from the computer if he wants to, 
+then go back to work when his favorite music finishes to play.
---
+++ music-break.py
@@ -0,0 +1,11 @@
+#Project I: Take a Music Break 
+
+"""
+Do you know a friend who works too many hours? We will write a program that schedules breaks throughout
+the day -- reminding your friend to listen to his favorite music in the web browser, get up and dance to 
+it or just walk away from the computer if he wants to, then go back to work when his favorite music finishes to play.
+
+"""
+
+def music_break(break_duration, number_of_breaks):
+    pass
