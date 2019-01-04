a3c37793bfedf59196bea217365b9fd847d98f56
patricksile <patricksile@gmail.com>
2019-01-04 10:13:36 +0100

    Solved with While loop

--- music-break.py
+++ music-break.py
@@ -7,5 +7,20 @@ it or just walk away from the computer if he wants to, then go back to work when
 
 """
 
-def music_break(break_duration, number_of_breaks):
-    pass
+# def music_break(break_duration, number_of_breaks):
+#     pass
+
+import webbrowser, time
+
+def music_break(nb_breaks, break_duration, music_url):
+    
+    counter = 0 # Initializing counter
+    while counter < nb_breaks:
+        webbrowser.open(music_url)
+        time.sleep(break_duration) # Delaying my code for some seconds
+        counter += 1 
+    
+
+# Test Zone
+
+music_break(3, 25, "https://www.youtube.com/watch?v=zOWJqNPeifU")
